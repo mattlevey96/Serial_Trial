ce59cbad46f764069c8371c924c8a014abaa3516
Matt Levey <mattlevey@Matts-MacBook-Pro.local>
2017-09-30 16:40:00 -0400

    trying shit

---
+++ header.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   header.h
+ * Author: mattlevey
+ *
+ * Created on September 30, 2017, 4:39 PM
+ */
+
+#ifndef HEADER_H
+#define	HEADER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HEADER_H */
+
--- nbproject/Package-default.bash
+++ nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Serial_Trial.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Serial_Trial.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=serialtrial/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/SerialTrial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=SerialTrial.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=serialtrial.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/serialtrial/bin
+makeDirectory ${TMPDIR}/serialtrial.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/serialtrial.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/serialtrial.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/serialtrial.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/serialtrial.x.tar *
 checkReturnCode
 
 # Cleanup
